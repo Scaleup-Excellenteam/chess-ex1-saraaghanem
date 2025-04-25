08ca6b1dfc18c5b7df18b507da32ca6bf27319b7
saraaghanem <saraa.gh117@gmail.com>
2025-04-25 14:48:10 +0300

    Add updated Chess files - exercise 1

--- Chess/src/King.cpp
+++ Chess/src/King.cpp
@@ -2,4 +2,23 @@
 // Created by MyPC on 24/04/2025.
 //
 
-#include "King.h"
+#include "../include/King.h"
+#include "../include/King.h"
+#include <cmath>
+
+King::King(bool isWhite) : white(isWhite) {}
+
+bool King::isWhite() const { return white; }
+
+char King::symbol() const { return white ? 'K' : 'k'; }
+
+bool King::isLegalMove(int srcRow, int srcCol, int destRow, int destCol,
+                       const std::vector<std::vector<Piece*>>& board) const {
+    int dRow = std::abs(destRow - srcRow);
+    int dCol = std::abs(destCol - srcCol);
+    if (dRow <= 1 && dCol <= 1) {
+        if (board[destRow][destCol] == nullptr || board[destRow][destCol]->isWhite() != white)
+            return true;
+    }
+    return false;
+}
